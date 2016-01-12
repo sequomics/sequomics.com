6008d24c2f22f6ece254e5cf93c01cf7aa385f04
Sequømics <sequomics@users.noreply.github.com>
2016-01-12 13:06:56 +0530

    Initial commit.🇮🇳
    Build & Development, v0.0.1 for Sequømics®.
    Usage: sh build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,17 @@
 # Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
 # Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.
 # ====================================================================
+
+# To exit whenever anything returns a non-zero return value.
+set -e
+echo "========================== Sequømics® =========================="
+echo "1. Ruby Version:"
+ruby -v
+echo "2. Compass Version:"
+compass version
+echo "3. Compile with Compass:"
+compass compile
+echo "4. Compile along with sourcemap by Compass:"
+compass compile --sourcemap
+echo "5. All are done, v0.0.1 for Sequømics®."
+echo "================================================================"
